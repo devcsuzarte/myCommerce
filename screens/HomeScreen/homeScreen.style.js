f6c31b5da60c55479f4d6924885c512b7679654b
Claudio Jr <85254941+devcsuzarte@users.noreply.github.com>
2024-03-21 13:59:58 -0300

    UI Update - Bottom Bar v2.1

--- screens/HomeScreen/homeScreen.style.js
+++ screens/HomeScreen/homeScreen.style.js
@@ -110,9 +110,21 @@ export const s = StyleSheet.create({
 
      backgroundColor: "white",
 
-     paddingHorizontal: 20,
-     paddingTop: 5,
-     paddingBottom: 20,
+     paddingVertical: 10,
+     paddingHorizontal: 22,
+     marginHorizontal: 35,
+     marginBottom: 25,
+     borderRadius: 50,
+
+      shadowColor: "#000",
+      shadowOffset: {
+      width: 0,
+      height: 1,
+        },
+        shadowOpacity: 0.22,
+        shadowRadius: 2.22,
+
+        elevation: 3,
 
    },
 
